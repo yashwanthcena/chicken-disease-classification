2e570eabc99cdfb8612bb7980f21c4d7b5c0dd00
yashwanthcena <yashwanthcena30@gmail.com>
2023-06-07 17:31:41 +0530

    added requirements.txt

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description =f.read()
+
+
+__version__ ="0.0.0"
+
+REPO_NAME= "chicken-disease-classification"
+AUTHOR_USER_NAME = "yashwanthcena"
+SRC_REPO ="CnnClassifier"
+AUTHOR_EMAIL ="yashwanthmadugula25@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small Python Package for CNN app",
+    long_description=long_description,
+    long_description_content= "text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
