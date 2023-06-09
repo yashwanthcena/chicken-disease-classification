5d6b8f5ee9da5118b2644b8ffd889c58cbd9b51f
yashwanthcena <yashwanthcena30@gmail.com>
2023-06-09 14:05:37 +0530

    data ingestion completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # chicken-disease-classification
+
+#workflows
+
+update config.yaml
+update secret.yaml
+update params.yaml
+update the entity
+update the configuration manager in the src config
+update the components
+update the pipeline
+update the main.py
+update the dvc.yaml
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from CnnClassifier import logger
-logger.info("Welcome to my first end-to-end Project")
+from CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion= DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e    
